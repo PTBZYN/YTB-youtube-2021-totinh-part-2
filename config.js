a2096d5e3f0600166ccd17859345e1f42a330487
PTBZYN <119863589+PTBZYN@users.noreply.github.com>
2022-12-05 21:40:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
-    reasonPlaceholder: 'Lý do',
+    introTitle: '🖐🖐🖐 Chào Bé Zyn của anh ! 🖐🖐🖐',
+    introDesc: `Anh cảm ơn em trong 6 tháng qua đã đến và yêu anh, thật lòng mà nói a thấy a rất may mắn vì anh có được em`,
+    btnIntro: 'Tiếp nè',
+    title: 'Anh nhiều lúc rất muốn đấm anh vì anh không thể đến bên em sớm hơn và lo cho em được nhiều hơn và cùng em chia sẻ mỗi khi em buòn bực hay đưa ra lời khuyên cho em ? 😙',
+    desc: 'Đây không giống như 1 lời để ôn kỉ niệm mà giống như lời cảm ơn của anh dành cho em hơn, mong em đọc tiếp :3 ',
+    btnYes: 'cảm ơn bé đã nhấn vào 💖💖💖',
+    btnNo: '💔 Nhấn cái này hộ cái ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh và đã thay đổi vì anh vậy ạ 😊😊',
+    reasonPlaceholder: 'Viết vô đây nè',
     btnReply: 'Gởi ! 💌💌',
     reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
+    mess: 'Anh biết mà, anh mong a sẽ được nhiều lần làm cho em vui vẻ, nhiều lần đc làm web cho em 🥰. thật ra a muốn a và e cùng làm vào lúc nãy đó, nhưng mà em không vui nên a.... 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha hoặc nếu mệt thì để mai ạ. 💬💬',
+    btnAccept: '💖',
+    messLink: 'https://www.facebook.com/phambinh1810'
 }
